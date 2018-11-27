e7afb11ded0d0a64d3676c25bee159c98beb8549
Natasha Pavlovikj <npavlovikj2@unl.edu>
2018-11-27 03:52:12 -0600

    Update recipe

--- recipes/libstatgen/build.sh
+++ recipes/libstatgen/build.sh
@@ -4,10 +4,6 @@ export CFLAGS="-I$PREFIX/include -I${SRC_DIR}/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include:${SRC_DIR}/include
 
-#export CXXFLAGS="$CXXFLAGS -std=c++11 -I${PREFIX}/include"
-#export CXX_FLAGS="${CXX_FLAGS} -std=c++11 -I${PREFIX}/include"
-
 make
-make test
 cp libStatGen*.a ${PREFIX}/lib
-
+cp include/* ${PREFIX}/include
