1f00523bd29ce22f59593bde31ecbbc6d40c78d1
Martin Steinegger <themartinsteinegger@gmail.com>
2019-02-25 16:55:04 -0500

    HHsuite: Add -DHAVE_MPI=0

--- recipes/hhsuite/build.sh
+++ recipes/hhsuite/build.sh
@@ -5,6 +5,6 @@ export CXX=g++
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_SSE2=1 ..
+cmake -DHAVE_MPI=0 -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DHAVE_SSE2=1 ..
 make -j 2
 make install
